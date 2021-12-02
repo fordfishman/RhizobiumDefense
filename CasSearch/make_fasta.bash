990df3a17181637f1fae7a110af38d62ee46b00b
Ford Fishman <ffishman@iu.edu>
2021-12-02 16:00:36 -0500

    blast scripts

--- CasSearch/make_fasta.bash
+++ CasSearch/make_fasta.bash
@@ -4,13 +4,14 @@
 # Set pathing
 sourcepath=/N/slate/ffishman/RhizobiumDefense/CasProfiles
 
-output=/N/slate/ffishman/RhizobiumDefense/cas_profiles.fasta
+output=/N/slate/ffishman/RhizobiumDefense/output.fasta
 
 # remove output file if it exists
-if [ -d $output ]; then rm $output; fi
+if [ -f $output ]; then rm $output; fi
 
 # run python script on all cas profiles (.sr files)
 for file in $sourcepath/*.sr
 do
+    # python3 make_fasta.py -f "$file" -o $output
     python3 make_fasta.py -f "$file"
 done
